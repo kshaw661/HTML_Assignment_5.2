9bf5e39fb11e8d678ccda657f1bcb0009b2b8330
kshaw661 <risk.always@gmail.com>
2017-04-30 08:49:25 +0530

    Add files via upload

---
+++ app_2.js
@@ -0,0 +1,11 @@
+var obj = {
+    name: "Kunal Shaw",
+    age: 32,
+	dob:'12th March, 1985',
+	pob:'Kolkata / West Bengal',
+	
+};
+console.log(obj.name);
+console.log(obj['age']);
+console.log(obj['dob']);
+console.log(obj.pob);
